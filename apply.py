acad810ef81b24814381c7fb3fa8455114b2ca98
M12 <121401817+MISSAOUI1920@users.noreply.github.com>
2024-07-17 17:51:48 +0100

    Update apply.py

--- apply.py
+++ apply.py
@@ -5,11 +5,10 @@ import streamlit as st
 
 # Sample text for summarization
 text = """
-Le web scraping, parfois appelé harvesting ou en français moissonnage1, est une technique de « récupération et organisation automatisées des données Web » ; c'est la principale forme de data mining et d'extraction des données de sites web, via un script ou d'un programme. Il vise à capter des données, pour les transformer et/ou les réutiliser dans un autre contexte comme l'enrichissement de bases de données, le référencement2 ou l'exploration de données, ou l'apprentissage profond pour une intelligence artificielle. L'objectif est souvent commercial, mais parfois scientifique ou politique.
-"""
+Web scraping, web harvesting, or web data extraction is data scraping used for extracting data from websites.[1] Web scraping software may directly access the World Wide Web using the Hypertext Transfer Protocol or a web browser. While web scraping can be done manually by a software user, the term typically refers to automated processes implemented using a bot or web crawler. It is a form of copying in which specific data is gathered and copied from the web, typically into a central local database or spreadsheet, for later retrieval or analysis."""
 
 # Parsing the text
-parser = PlaintextParser.from_string(text, Tokenizer("french"))
+parser = PlaintextParser.from_string(text, Tokenizer("english"))
 
 # Summarizing the text
 summarizer = LsaSummarizer()
